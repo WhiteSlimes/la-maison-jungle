72e2071491fa89596eba90bed93dddc7331f8dac
WhiteSlimes <43409869+WhiteSlimes@users.noreply.github.com>
2023-07-13 14:57:08 +0200

    ~ Versioning 1.2~
     - Add ShoppingList/CareScale

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,19 +1,31 @@
-const plantList = [
-    'monstera',
-    'ficus lyrata',
-    'photos argenté',
-    'yucca',
-    'palmier',
-    'tournesol'
-]
+import { plantList } from "../datas/plantList"
+import '../styles/shoppingList.css'
+import CareScale from "./CareScale"
 
 function ShoppingList(){
+    const categories = plantList.reduce(
+        (acc, plant) =>
+            acc.includes(plant.category) ? acc : acc.concat(plant.category),
+            []
+    )
     return(
-        <ul>
-            {plantList.map((plant) => (
-                <li>{plant}</li>
-            ))}
-        </ul>
+        <div>
+            <ul>
+                {categories.map((cat) => (
+                    <li key={cat}>{cat}</li>
+                ))}
+            </ul>
+            <ul className="lmj-plant-list">
+                {plantList.map((plant) => (
+                    <li key={plant.id} className="lmj-plant-item">
+                        {plant.name}
+                        {plant.isSpecialOffer && <div className="lmj-sales">Soldes</div>}
+                        <CareScale careType='water' scaleValue={plant.water} />
+                        <CareScale careType='light' scaleValue={plant.light} />
+                    </li>
+                ))}
+            </ul>
+        </div>
     )
 }
 
